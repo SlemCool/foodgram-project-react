163e4733ed942899d4eb1e58bef3f2950ecc3ac1
Андрей Кагадий <kagadi.a@gmail.com>
2023-06-05 16:18:28 +0300

    modified settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -101,6 +101,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': [
+        'api.pagination.CustomPagination',
+    ],
+    'PAGE_SIZE': 6,
+    'SEARCH_PARAM': 'name',
 }
 
 DJOSER = {
