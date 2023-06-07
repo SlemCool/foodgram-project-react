47e25f916e11b38ed5ed7d7271ba993c4a2d32c6
Андрей Кагадий <kagadi.a@gmail.com>
2023-06-07 16:09:02 +0300

    modified:   backend/api/serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -229,15 +229,19 @@ class RecipeReadSerializer(ModelSerializer):
 
     def get_is_favorited(self, obj):
         user = self.context.get('request').user
-        return (user.is_authenticated and Favorite.objects.filter(
-            user=user, recipe=obj).exists()
-        )
+        return (user.is_authenticated
+                and Favorite.objects.filter(
+                    user=user,
+                    recipe=obj).exists()
+                )
 
     def get_is_in_shopping_cart(self, obj):
         user = self.context.get('request').user
-        return (user.is_authenticated and ShoppingCart.objects.filter(
-            user=user, recipe=obj).exists()
-        )
+        return (user.is_authenticated
+                and ShoppingCart.objects.filter(
+                    user=user,
+                    recipe=obj).exists()
+                )
 
 
 class IngredientInRecipeWriteSerializer(ModelSerializer):
